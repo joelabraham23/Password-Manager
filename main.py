b2a5cb5cc9794aa277408940573efd472d2ed50d
joelabraham <z5310056@ad.unsw.edu.au>
2023-01-05 19:58:11 +1100

    Created starting layout for pw manager with the functions that will be required

---
+++ main.py
@@ -0,0 +1,2 @@
+"""Password manager utilising sqlite3 
+for the database and tkinter to provide a GUI for users."""
---
+++ pw_manager.py
@@ -0,0 +1,20 @@
+
+def display_all():
+    """Displays all the records (login details) from the table"""
+    pass
+
+def add_pass():
+    """Add a new record into the database"""
+    pass
+
+def show_pass():
+    """Show all the info related to a specifcied record"""
+    pass
+
+def delete_pass():
+    """Delete a record from the table """
+    pass
+
+def modify_pass():
+    """Modify the password for a record in table"""
+    pass

a13b90081abfdf56dfa4f5756da56dfab0e05191
joelabraham <z5310056@ad.unsw.edu.au>
2023-01-05 20:25:24 +1100

    Evaluated all the functions regarding the connection to db

--- pw_manager.py
+++ pw_manager.py
@@ -1,29 +1,65 @@
 import sqlite3
 import tkinter
 from cryptography.fernet import Fernet
+from key_creator import key_creator
 
 def create_db():
-    """Only use this functino when it is the first time running
+    """Only use this function when it is the first time running
     the password manager"""
     conn = sqlite3.connect("passwordManager.db")
     c = conn.cursor()
     c.execute("CREATE TABLE login_details(website text, username text, password text)")
+    key_creator()
 
 def display_all():
     """Displays all the records (login details) from the table"""
-    pass
+    conn = sqlite3.connect("passwordManager.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM login_details")
+    records = c.fetchall()
+    conn.commit()
+    conn.close()
+    return records
 
-def add_pass():
+
+def add_pass(website, username, encrypted_password):
     """Add a new record into the database"""
-    pass
+    conn = sqlite3.connect("passwordManager.db")
+    c = conn.cursor()
 
-def show_pass():
-    """Show all the info related to a specifcied record"""
-    pass
+    c.execute("INSERT INTO login_details VALUES (:website, :username, :password)", 
+    {
+        "website": website, 
+        "username": username,
+        "password": encrypted_password,
+    }
+    )
+    conn.commit()
+    conn.close()
 
-def delete_pass():
-    """Delete a record from the table """
-    pass
+
+def show_pass(website):
+    """Show the login details from the table given its website"""
+    conn = sqlite3.connect("passwordManager.db")
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM login_details WHERE website = (?)",
+    (website))
+    items = c.fetchall()
+    conn.commit()
+    conn.close()
+    return items
+
+
+def delete_pass(website):
+    """Delete the login details from table given its website """
+    conn = sqlite3.connect("passwordManager.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM login_details where website = (?)",
+    (website))
+    conn.commit()
+    conn.close()
+    
 
 def modify_pass():
     """Modify the password for a record in table"""

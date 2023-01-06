4ef7ea97a1482fe17cd0009d5536b712377caf41
joelabraham <z5310056@ad.unsw.edu.au>
2023-01-06 18:06:26 +1100

    Created the GUI for adding a new login and showing a specific login

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from pw_db_manager import create_db, display_all, add_login, show_login, delete_
 import tkinter
 from cryptography.fernet import Fernet
 
-def tk_display_all(key):
+def tk_display_all():
     login_dets_columns = "WEBSITE ------ USERNAME ----- PASSWORD\n"
     columns_label = tkinter.Label(root, text=login_dets_columns)
     columns_label.grid(row=13, column=0, columnspan=2)
     idx = 14
     blank_label = tkinter.Label(root, text='\n').grid(row=idx, column=0, ipadx=100)
+    f_key = Fernet(key)
     for record in display_all():
-        f_key = Fernet(key)
         decrypted_pw = f_key.decrypt(record[2]).decode()
         login_dets_label=tkinter.Label(root, text=f"{record[0]}     {record[1]}     {decrypted_pw}\n")
         login_dets_label.grid(row=idx, column=0, columnspan=2)
@@ -19,13 +19,28 @@ def tk_display_all(key):
 
 
 def tk_add_login():
-    pass
+    website_input = website_field.get()
+    username_input = username_field.get()
+    password_input = password_field.get()
 
-def tk_add_login():
-    pass
+    f_key = Fernet(key)
+    encoded_password = f_key.encrypt(password_input.encode())
+
+    add_login(website_input, username_input, encoded_password)
+    website_field.delete(0, END)
+    username_field.delete(0, END)
+    password_field.delete(0, END)
+
 
 def tk_show_login():
-    pass
+    website_input = website_field.get()
+    f_key = Fernet(key)
+    for record in show_login(website_input):
+        
+        decrypted_password = f_key.decrypt(record[2]).decode()
+        login_dets_label = tkinter.Label(root, text = f"{record[1]}     {decrypted_password}\n")
+        login_dets_label.grid(row=8, column=0, columnspan=4, ipadx=100)
+    website_field.delete(0, END)
 
 def tk_delete_login():
     pass
@@ -34,14 +49,12 @@ def tk_modify_login():
     pass
 
 
-if __name__ == "__main__":
+# If running password manager for the first time uncomment out this line
+create_db()
 
-    # If running password manager for the first time uncomment out this line
-    create_db()
+root = tkinter.Tk()
+root.title("Password manager")
+key = ""
+with open("key.txt", "rb") as key_file:
+    key = key_file.read()
 
-    root = tkinter.Tk()
-    root.title("Password manager")
-    key = ""
-    with open("key.txt", "rb") as key_file:
-        key = key_file.read()
-    
